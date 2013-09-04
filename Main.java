2fcf4ea03ba98196208b6973034ea0b258287c5f
ankitaurfrnd <ankitaurfrnd@gmail.com>
2013-09-04 23:37:33 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,65 @@
+package dataStructures;
+
+public class Main {
+
+	public static void main(String[] args) {
+		QueueArray queue= new QueueArray();
+		
+		System.out.println("------------Stack by Array---------------");
+		for(int i=1; i<6;i++)
+		 queue.insert(i); 		//inserting elements
+		
+		queue.display();		//displays elements
+				
+		for(int i=1; i<3;i++)
+			 queue.remove(); 	//removing elements
+		
+		queue.display();
+		
+		for(int i=11; i<18;i++)
+			 queue.insert(i);
+		queue.display();
+		
+		System.out.println("\n------------Queue by Array---------------");
+		
+		StackArray stack=new StackArray();
+		for(int i=1; i<6;i++)
+			 stack.push(i); 		//inserting elements
+		stack.display();
+		for(int i=1; i<3;i++)
+			 stack.pop(); 	//removing elements
+		stack.display();
+		for(int i=5; i<14;i++)
+			 stack.push(i); 		//inserting elements
+		stack.display();
+		for(int i=1; i<12;i++)
+			 stack.pop(); 	//removing elements
+		stack.display();
+		
+		System.out.println("\n------------Queue by List------------------");
+		
+		QueueList qlist=new QueueList();
+		for(int  i=2; i<10;i++)
+			qlist.insert(i);
+		qlist.display();
+		for(int  i=1; i<11;i++)
+			qlist.remove();
+			
+		qlist.display();
+		
+	
+		
+		System.out.println("\n------------Stack by List------------------");
+		StackList stacklist = new StackList();
+		for(int  i=2; i<10;i++)
+		stacklist.push(i);
+		stacklist.display();
+		for(int  i=2; i<10;i++)
+			stacklist.pop();
+		stacklist.display();
+		stacklist.pop();
+		
+	
+	}
+
+}
